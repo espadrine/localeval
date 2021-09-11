4ea35162d963fca56ea63af07f9c9f295b4264ca
Thaddee Tyl <thaddee.tyl@gmail.com>
2021-09-11 19:23:58 +0200

    Add tests for example protections granted for Node

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,53 @@
-describe('localeval', function () {
-  it('should work in a browser', function () {
-    if (!localeval) {
-      throw new Error('localeval is not defined');
-    }
+var assert = require('assert');
+var localeval = require('..');
+describe('synchronous localeval', function() {
+  it('basics', function() {
+    assert.equal(localeval(`1+1`), 2);
+
+    var foo = 1;
+    [
+      `foo = 'leak'`,
+      `this.foo = 'leak'`,
+      `Function("this.foo = 'leak'")()`,
+      `eval("foo = 'leak'")`,
+    ].forEach(attempt => localeval(attempt));
+    assert.equal(foo, 1);
+
+    assert.throws(() =>
+      localeval(`this.constructor.constructor('return foo')()`),
+      /foo is not defined/);
+
+    localeval(`String.prototype.slice = function() { return 'leaked'; };`);
+    assert.equal('nice'.slice(1), 'ice');
+
+    localeval(`String.prototype.leak = function() { return 'leak'; }`);
+    assert.equal(String.prototype.leak, undefined);
+
+    localeval(`([]).__proto__.push = function(a) { return "leaked"; };`);
+    assert.equal([].push(1), 1);
+
+    localeval(`String.fromCharCode = function() { return 'leaked'; };`);
+    assert.equal(String.fromCharCode(42), '*');
+
+    localeval(`delete Number.parseInt`);
+    assert.equal(Number.parseInt('1337'), 1337);
+
+    localeval(`({}).constructor.getOwnPropertyNames = function(){return 'leak';}`);
+    assert.deepEqual(Object.getOwnPropertyNames(1), []);
+  });
+
+  it('timeout', function() {
+    assert.throws(() => localeval(`for (;;) {}`, {}, 1), /ETIMEDOUT/);
+
+    assert.throws(() => localeval(`function(a, n) {` +
+          `if (n > 0) return a + a;` +
+          `else return a;` +
+        `}("leak", 1e9);`, {}, 1),
+      /ETIMEDOUT/);
+
+    assert.throws(() =>
+      localeval(`this.constructor.constructor('process.exit(0)')()`),
+      /Failed localeval execution/);
   });
 });
+
